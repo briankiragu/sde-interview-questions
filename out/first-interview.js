011b0049faf98d5997dbc3cf2b52b924278dd31a
Brian Kiragu <bkariuki@hotmail.com>
2023-12-04 14:26:39 +0300

    Initial commit

--- out/first-interview.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const global_dict = ["PRIME", "PREMS", "DSEMS", "PREWS", "PRDSS", "ESEMS"];
-const get_hints = (green, yellow, red) => { };
-const output = get_hints(["P", "R", "-", "-", "-"], ["-", "-", "M", "-", "-"], ["A", "Z"]);
-//# sourceMappingURL=first-interview.js.map
--- src/first-interview.ts
+++
@@ -1,9 +0,0 @@
-const global_dict = ["PRIME", "PREMS", "DSEMS", "PREWS", "PRDSS", "ESEMS"];
-
-const get_hints = (green: string[], yellow: string[], red: string[]) => {};
-
-const output = get_hints(
-  ["P", "R", "-", "-", "-"],
-  ["-", "-", "M", "-", "-"],
-  ["A", "Z"]
-);
