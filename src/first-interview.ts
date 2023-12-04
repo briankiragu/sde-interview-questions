b2e54b6d7d26b4e79187113543510e0e22b07a07
Brian Kiragu <bkariuki@hotmail.com>
2023-12-04 14:25:30 +0300

    Initial commit

---
+++ src/first-interview.ts
@@ -0,0 +1,9 @@
+const global_dict = ["PRIME", "PREMS", "DSEMS", "PREWS", "PRDSS", "ESEMS"];
+
+const get_hints = (green: string[], yellow: string[], red: string[]) => {};
+
+const output = get_hints(
+  ["P", "R", "-", "-", "-"],
+  ["-", "-", "M", "-", "-"],
+  ["A", "Z"]
+);
