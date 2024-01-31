fb2a5140e9bfb3243baf6f927f2660365840582f
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-01-31 11:05:42 +0300

    feat: Implement unique email addresses function

--- src/leet-code/unique-email-addresses.ts
+++ src/leet-code/unique-email-addresses.ts
@@ -1,18 +1,23 @@
 const numUniqueEmails = (emails: string[]): number => {
-  const email: string = emails.at(0);
+  const processedEmails = emails.map((email) => {
+    // Split the string into two parts; local name and domain name;
+    const [local, domain] = email.split('@');
 
-  // Split the string into two parts; local name and domain name;
-  const [local, domain] = email.split('@');
+    // Split the local name by '+' and discard the remainder.
+    let [relevant] = local.split('+', 1) as unknown as string;
 
-  console.dir({ local, domain });
+    // Replace the '.' with empty space.
+    relevant = relevant.replaceAll('.', '');
+
+    // Combine the email address parts.
+    return `${relevant}@${domain}`;
+  });
+
+  // Create a set from the processed emails.
+  const uniqueProcessedEmails = new Set(processedEmails);
+
+  // Return the size of the set.
+  return uniqueProcessedEmails.size;
 };
 
 export default numUniqueEmails;
-
-const emails: string[] = [
-  'test.email+alex@leetcode.com',
-  'test.e.mail+bob.cathy@leetcode.com',
-  'testemail+david@lee.tcode.com',
-];
-
-numUniqueEmails(emails);
