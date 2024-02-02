173fa723f7eb95cf4b37e1fedc11a9efdb9dde70
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-02 10:05:12 +0300

    feat: Implement function to find first relative prime in a set

--- src/alx/prime_game.py
+++ src/alx/prime_game.py
@@ -33,5 +33,72 @@ Ben wins because there are no prime numbers for Maria to choose
 """
 
 
-def prime_game():
+def is_winner(x: int, nums: list[int]) -> str | None:
     """Implementation of the Prime Game"""
+    # Set of players.
+    score: dict[str, int] = {"Maria": 0, "Ben": 0}
+
+    # Loop through the rounds.
+    for i in range(x):
+        # Set the current player.
+        # playerIndex: int
+
+        # Create the set of numbers for this round.
+        number_set: list[int] = list(range(1, nums[i] + 1))
+
+        prime: int | None = find_prime(number_set)
+
+        # Check for prime numbers in the set.
+
+        # Return false if none are found.
+        #
+
+        # Get the multiples of the prime number.
+        #
+
+        # Remove the prime number and its multiples from the the set.
+        #
+
+        # Return the subset.
+        #
+        pass
+
+    # If no winner, return None.
+    return None
+
+
+def find_prime(nums: list[int]) -> int | None:
+    """Iterate through a set of integers and return the first 'prime' number"""
+    prime: int | None = None
+
+    for i, num in enumerate(nums):
+        # 1 is not a prime number, skip it.
+        if num == 1:
+            continue
+
+        # If a prime number was found, break out of the loop.
+        if prime:
+            break
+
+        # Get a subset of all the numbers before the current index.
+        divisors: list[int] = nums[:i]
+
+        # Tentatively assume this number is prime.
+        prime = num
+
+        for divisor in divisors:
+            # If the divisor is 1, don't test for it.
+            if divisor == 1:
+                continue
+
+            # If any number (except 1) successfully divides it (mod is 0),
+            # it is not prime.
+            if num % divisor == 0:
+                prime = None
+                break
+
+    return prime
+
+
+if __name__ == "__main__":
+    is_winner(3, [4, 5, 1])
