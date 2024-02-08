aef6410aa71037ad26343d83f722d685111da9ca
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-08 14:18:31 +0300

    feat: Copy the question

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,4 @@
-import numUniqueEmails from './leet-code/unique-email-addresses';
+import numSquares from './leet-code/perfect-squares';
 
-const emails1: string[] = [
-  'test.email+alex@leetcode.com',
-  'test.e.mail+bob.cathy@leetcode.com',
-  'testemail+david@lee.tcode.com',
-];
-
-const emails2: string[] = [
-  'a@leetcode.com',
-  'b@leetcode.com',
-  'c@leetcode.com',
-];
-
-const uniqueEmails1 = numUniqueEmails(emails1);
-const uniqueEmails2 = numUniqueEmails(emails2);
-
-console.dir({ uniqueEmails1, uniqueEmails2 });
+const result = numSquares(8);
+console.dir(result);
