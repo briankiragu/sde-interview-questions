6a1ef3013eb232f1b4105df182f793c257eee111
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-13 13:59:15 +0300

    feat: Add test cases

---
+++ tests/leet-code/odd-even-jumps.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'bun:test';
+import oddEvenJumps from '../../src/leet-code/odd-even-jump';
+
+describe('odd-even-jump', () => {
+  it('Returns 2', () => {
+    expect(oddEvenJumps([10, 13, 12, 14, 15])).toBe(2);
+  });
+
+  it('Returns 3', () => {
+    expect(oddEvenJumps([2, 3, 1, 1, 4])).toBe(3);
+  });
+
+  it('Returns 3', () => {
+    expect(oddEvenJumps([5, 1, 3, 4, 2])).toBe(3);
+  });
+});
