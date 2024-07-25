7827fd75b865540fcbc9953717ece03aafaa0254
Brian K. Kiragu <bkariuki@hotmail.com>
2024-07-25 21:30:28 +0300

    microsoft: Solved Prime Decomposition (#9)
    * microsoft: Solved Prime Decomposition
    
    * test: Add test case to prime decomposition for large 'x'

---
+++ tests/microsoft/prime-decomposition.spec.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect } from "bun:test";
+import primeDecomposition from "../../src/microsoft/prime-decomposition";
+
+describe("Prime decomposition", () => {
+  test("7 returns [7]", () => {
+    expect(primeDecomposition(7)).toStrictEqual([7]);
+  });
+
+  test("21 returns [3,7]", () => {
+    expect(primeDecomposition(21)).toStrictEqual([3, 7]);
+  });
+
+  test("28 returns [2,2,7]", () => {
+    expect(primeDecomposition(28)).toStrictEqual([2, 2, 7]);
+  });
+
+  test("30 returns [2,3,5]", () => {
+    expect(primeDecomposition(30)).toStrictEqual([2, 3, 5]);
+  });
+
+  test("32 returns [2,2,2,2,2]", () => {
+    expect(primeDecomposition(32)).toStrictEqual([2, 2, 2, 2, 2]);
+  });
+
+  test("135237 returns [3,61,739]", () => {
+    expect(primeDecomposition(135237)).toStrictEqual([3, 61, 739]);
+  });
+});
