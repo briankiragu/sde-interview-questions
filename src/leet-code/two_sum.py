29c33d881bd72ba6eec72c80271454e90ca58bd4
Brian K. Kiragu <briankiragu@microsoft.com>
2026-06-09 10:07:49 +0300

    feat: Implement removeDuplicates functions and update .editorconfig for consistent indentation

---
+++ src/leet-code/two_sum.py
@@ -0,0 +1,31 @@
+﻿class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[list[int]]:
+        i:int = 0
+        j:int = i+1
+        results:list[list[int]] = []
+
+        while (i < len(nums)):
+            if (nums[i] > target):
+                break
+
+            print(f"Comparing nums[{i}]:")
+
+            while (j < len(nums)):
+                if (nums[j] > target):
+                    break
+
+                print(f"Comparing nums[{i}]: {nums[i]} with nums[{j}]: {nums[j]}")
+
+                if (nums[i] + nums[j] == target):
+                    print(f"Match found! nums[{i}] ({nums[i]}) + nums[{j}] ({nums[j]}) = ({target})")
+                    results.append([i,j])
+                j = j+1
+
+            i = i+1
+            j = i+1
+
+        return results
+
+
+sol1 = Solution().twoSum([1, 4, 6, 7, 9, 13, 16, 45], 10)
+print(sol1)
